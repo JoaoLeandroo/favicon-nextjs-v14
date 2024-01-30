b192d26981978c962a709b237e97a49eed64bc34
joaoleandro <new.joaoleandro@gmail.com>
2024-01-30 16:45:53 -0300

    favicon

---
+++ app/(site)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="selection:bg-green-600 selection:rounded-lg h-screen flex justify-center items-center">
+      <p className="text-4xl font-bold">TESTANDO FAVICON NEXTJS 14</p>
+    </div>
+  )
+};
+
+export default Home;
